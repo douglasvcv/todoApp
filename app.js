4fffeaaa4b4368d740d00dc40af258821cd0affe
douglasvcv <douglasvitor1233@gmail.com>
2025-04-21 23:40:43 -0300

    feat: Finishing app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const todoForm = document.querySelector("form")
 const todoInput = document.getElementById("todo-input")
 const todoList = document.getElementById("todo-list")
 
-const allTodos= []
+let allTodos= getTodos()
+updateTodoList()
 
 todoForm.addEventListener("submit", (e)=>{
     e.preventDefault()
@@ -14,7 +15,11 @@ function addTodo(){
     const todoText = todoInput.value.trim()
     
     if(todoText.length >0){
-        allTodos.push(todoText)
+        const todoObject = {
+            text: todoText,
+            completed: false
+        }
+        allTodos.push(todoObject)
         updateTodoList()
         todoInput.value = ""
     }
@@ -30,6 +35,7 @@ function updateTodoList(){
 
 function createTodoitem(todo, todoIndex){
     const todoId = "todo-"+todoIndex
+    const todoText = todo.text
     const todoLI = document.createElement("li")
     todoLI.className = "todo"
     todoLI.innerHTML = `
@@ -38,16 +44,37 @@ function createTodoitem(todo, todoIndex){
                     <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
                 </label>
                 <label for="${todoId}" id="todo-text">
-                 ${todo}
+                 ${todoText}
                 </label>
                 <button id="delete-todo">
                     <svg fill="var(--secondary-color)"  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </button>
     `
+    let deleteButton = todoLI.querySelector('#delete-todo')
+    deleteButton.addEventListener("click", ()=>{
+        deleteTodoItem(todoIndex)
+    })
+    let checkbox = todoLI.querySelector("input")
+    checkbox.addEventListener("change", ()=>{
+        allTodos[todoIndex].completed = checkbox.checked
+        saveTodo()
+    })
+    checkbox.checked = todo.completed
+
     return todoLI
 }
 
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_, i)=> i!== todoIndex)
+    saveTodo()
+    updateTodoList()
+}
+
 function saveTodo(){
-    const jsonTodo = JSON.stringify(allTodos)
+    let jsonTodo = JSON.stringify(allTodos)
     localStorage.setItem("todos", jsonTodo)
+}
+function getTodos(){
+    let todoLocal= localStorage.getItem("todos") || []
+    return JSON.parse(todoLocal)
 }
